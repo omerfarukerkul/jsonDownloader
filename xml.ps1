0847a4cc3fa6ea89e3535682d01c240195e3eb69
omerfaruk <omerfaruk@dilisim.com>
2018-09-26 17:54:26 +0300

    xml parser update

---
+++ xml.ps1
@@ -0,0 +1,20 @@
+﻿ Function Get-FileName($initialDirectory)
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.filter = "XML (*.xml)| *.xml"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+}
+
+$XmLFileName = Get-FileName "C:\Users\ofe\Desktop"
+[xml]$XmLFile = Get-Content $XmLFileName
+
+$XmLBase = $XmLFile.databases.database.Get(0)
+
+$MySQLDatabaseName = [string]$XmLBase.name
+$MySQLTableName = [string]$XmLBase.tables.table.Get(0).name
+
+
